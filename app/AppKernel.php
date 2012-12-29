f1d67e63f974a034470854020b1fc4276f6096ac
Nanouk <j2b.bayle@gmail.com>
2012-12-29 17:32:37 +0100

    Ajout de MopaBootstrapBundle
    Signed-off-by: Nanouk <j2b.bayle@gmail.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new CEC\ExternSiteBundle\CECExternSiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
