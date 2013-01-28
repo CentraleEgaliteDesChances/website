12c0876c58ceccd2cac9a063ed9691bf8c14c1f6
Nanouk <j2b.bayle@gmail.com>
2013-01-28 21:38:01 +0100

    Création de MembreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
             new CEC\ExternSiteBundle\CECExternSiteBundle(),
+            new CEC\MembreBundle\CECMembreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
