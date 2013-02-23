9c22d70378e64da6018a9fd6a77289b4da4cf2f2
Nanouk <j2b.bayle@gmail.com>
2013-02-23 13:49:53 +0100

    Ajout de l'entité Seance et des relations entre entités dans le TutoratBundle

---
+++ src/CEC/TutoratBundle/Entity/EnseignantLyceeReference.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace CEC\TutoratBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EnseignantLyceeReference
+ */
+class EnseignantLyceeReference
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $annee;
+
+    /**
+     * @var string
+     */
+    private $role;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set annee
+     *
+     * @param integer $annee
+     * @return EnseignantLyceeReference
+     */
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+    
+        return $this;
+    }
+
+    /**
+     * Get annee
+     *
+     * @return integer 
+     */
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     * @return EnseignantLyceeReference
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string 
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return EnseignantLyceeReference
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    
+        return $this;
+    }
+    /**
+     * @var \CEC\TutoratBundle\Entity\Lycee
+     */
+    private $lycee;
+
+
+    /**
+     * Set lycee
+     *
+     * @param \CEC\TutoratBundle\Entity\Lycee $lycee
+     * @return EnseignantLyceeReference
+     */
+    public function setLycee(\CEC\TutoratBundle\Entity\Lycee $lycee = null)
+    {
+        $this->lycee = $lycee;
+    
+        return $this;
+    }
+
+    /**
+     * Get lycee
+     *
+     * @return \CEC\TutoratBundle\Entity\Lycee 
+     */
+    public function getLycee()
+    {
+        return $this->lycee;
+    }
+    /**
+     * @var \CEC\TutoratBundle\Entity\Enseignant
+     */
+    private $enseignant;
+
+
+    /**
+     * Set enseignant
+     *
+     * @param \CEC\TutoratBundle\Entity\Enseignant $enseignant
+     * @return EnseignantLyceeReference
+     */
+    public function setEnseignant(\CEC\TutoratBundle\Entity\Enseignant $enseignant = null)
+    {
+        $this->enseignant = $enseignant;
+    
+        return $this;
+    }
+
+    /**
+     * Get enseignant
+     *
+     * @return \CEC\TutoratBundle\Entity\Enseignant 
+     */
+    public function getEnseignant()
+    {
+        return $this->enseignant;
+    }
+}
