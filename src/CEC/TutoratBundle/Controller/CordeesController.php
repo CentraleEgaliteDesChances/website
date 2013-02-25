66c6ee44992fee27a6b4908152abd610510fd922
Nanouk <j2b.bayle@gmail.com>
2013-02-25 18:07:04 +0100

    Ajout du système d'ajout/suppression des lycées dans les cordées

--- src/CEC/TutoratBundle/Controller/CordeesController.php
+++ src/CEC/TutoratBundle/Controller/CordeesController.php
@@ -5,6 +5,7 @@ namespace CEC\TutoratBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use CEC\TutoratBundle\Form\Type\NomCordeeType;
+use CEC\MainBundle\Classes\AnneeScolaire;
 use CEC\TutoratBundle\Entity\Cordee;
 use CEC\TutoratBundle\Entity\Lycee;
 use CEC\TutoratBundle\Entity\ChangementCordeeLycee;
@@ -94,6 +95,28 @@ class CordeesController extends Controller
         });
         return $pivots;
     }
+    
+    /**
+     * Helper
+     * Effectue un changement de partenariat entre une cordée et un lycée.
+     * Si le lycée était partenaire, il ne l'est plus et vice-versa.
+     *
+     * @param Cordee $cordee: cordée
+     * @param Lycee $lycee: lycée
+     */
+    public function changerPartenariat(Cordee $cordee, Lycee $lycee)
+    {
+        $changement = new ChangementCordeeLycee();
+        $anneeScolaire = new AnneeScolaire();
+        $changement->setAnnee($anneeScolaire->getAnneeScolaire())
+            ->setLycee($lycee)
+            ->setCordee($cordee)
+            ->setAction( in_array($lycee, $this->getLyceesForCordee($cordee)) ? ChangementCordeeLycee::CHANGEMENT_ACTION_SUPPRESSION : ChangementCordeeLycee::CHANGEMENT_ACTION_AJOUT );
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($changement);
+        $em->flush();
+    }    
 
     /**
      * Affiche tous les lycées actifs
@@ -112,7 +135,7 @@ class CordeesController extends Controller
         $form = $this->createForm(new NomCordeeType(), $nouvelleCordee);
         
         if ($this->getRequest()->getMethod() == 'POST') {
-            $form->bindRequest($this->getRrequest());
+            $form->bindRequest($this->getRequest());
             if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
                 $em->persist($nouvelleCordee);
@@ -124,7 +147,7 @@ class CordeesController extends Controller
                 );
                 
                 return $this->redirect($this->generateUrl('editer_cordee', array(
-                    'id' => $nouvelleCordee->getId()
+                    'cordee' => $nouvelleCordee->getId()
                 )));
             }
         }
@@ -140,12 +163,12 @@ class CordeesController extends Controller
     /**
      * Affiche les lycées d'une cordée
      *
-     * @param integer $id: id de la cordée a afficher
+     * @param integer $cordee: id de la cordée a afficher
      */
-    public function voirAction($id)
+    public function voirAction($cordee)
     {
         $cordee = $this->getDoctrine()->getRepository('CECTutoratBundle:Cordee')
-            ->find($id);
+            ->find($cordee);
         $lycees = $this->getLyceesForCordee($cordee);
     
         return $this->render('CECTutoratBundle:Cordees:voir.html.twig', array(
@@ -156,12 +179,14 @@ class CordeesController extends Controller
     
     /**
      * Permet l'édition d'une cordée de la réussite
+     *
+     * @param integer $cordee: id de la cordée
      */
-    public function editerAction($id, Request $request)
+    public function editerAction($cordee)
     {
         // On récupère la cordée
         $cordee = $this->getDoctrine()->getRepository('CECTutoratBundle:Cordee')
-            ->find($id);
+            ->find($cordee);
             
         // On récupère les lycées de la cordée et les lycées sans cordée
         $lyceesCordee = $this->getLyceesForCordee($cordee);    // Lycées de la cordée
@@ -171,6 +196,12 @@ class CordeesController extends Controller
         {
             if ( count($this->getCordeesForLycee($lycee)) == 0 ) $lyceesInactifs[] = $lycee;
         }
+        
+        // On trie les lycées à afficher par ordre alphabétique
+        $lycees = array_merge($lyceesCordee, $lyceesInactifs);
+        usort($lycees, function($a, $b) {
+            return strcmp($a->getNom(), $b->getNom());
+        });
     
         // Formulaire de changement de nom de la cordée
         $nomNomForm = 'nom_form';
@@ -178,14 +209,13 @@ class CordeesController extends Controller
             ->createNamedBuilder($nomNomForm, new NomCordeeType(), $cordee)
             ->getForm();
             
-        if ($request->getMethod() == 'POST')
+        if ($this->getRequest()->getMethod() == 'POST')
         {
-            $nomForm->bindRequest($request);
-            var_dump($request);
+            $nomForm->bindRequest($this->getRequest());
             if (false and $nomForm->isValid()) {
                 $this->getDoctrine()->getEntityManager()->flush();
                 $this->get('session')->setFlash('success', 'Le nom de la cordée a bien été mise à jour.');
-                return $this->redirect($this->generateUrl('editer_cordee', array('id' => $id)));
+                return $this->redirect($this->generateUrl('editer_cordee', array('cordee' => $cordee)));
             }
         }
         
@@ -193,30 +223,49 @@ class CordeesController extends Controller
         return $this->render('CECTutoratBundle:Cordees:editer.html.twig', array(
             'cordee'        => $cordee,
             'nom_form'      => $nomForm->createView(),
-            'sources'       => $this->filterLyceesSources(array_merge($lyceesCordee, $lyceesInactifs)),
-            'pivots'        => $this->filterLyceesPivots(array_merge($lyceesCordee, $lyceesInactifs)),
+            'sources'       => $this->filterLyceesSources($lycees),
+            'pivots'        => $this->filterLyceesPivots($lycees),
             'lycees_cordee' => $lyceesCordee,
         ));
     }
     
     /**
-     * Désactive une cordee
+     * Bascule l'état d'un lycée pour une cordée
      *
-    public function desactiverAction($id)
+     * @param integer $cordee: id de la cordée
+     * @param integer $lycee: id du lycée
+     */
+    public function changerLyceeAction($cordee, $lycee)
     {
-        $references = $this->getDoctrine()->getRepository('CECTutoratBundle:CordeeLyceeReference')
-            ->findAllForCordeeIdAndYear($id);
+        // On récupère la cordée et le lycée
+        $doctrine = $this->getDoctrine();
+        $cordee = $doctrine->getRepository('CECTutoratBundle:Cordee')->find($cordee);
+        $lycee = $doctrine->getRepository('CECTutoratBundle:Lycee')->find($lycee);
         
-        $em = $this->getDoctrine()->getEntityManager();
-        array_map(function($ref) {
-            global $em;
-            $this->getDoctrine()->getEntityManager()->remove($ref);
-        }, $references);
-        $em->flush();
+        // On ajoute une changement de partenariat
+        $this->changerPartenariat($cordee, $lycee);
+                        
+        $this->get('session')->setFlash('success', 'Les modifications ont été enregistrés.');
+        return $this->redirect($this->generateUrl('editer_cordee', array('cordee' => $cordee->getId() )));
+    }
+    
+    /**
+     * Désactive une cordee
+     */
+    public function desactiverAction($cordee)
+    {
+        // Récupère la cordée et ses lycées
+        $cordee = $this->getDoctrine()->getRepository('CECTutoratBundle:Cordee')->find($cordee);
+        $lycees = $this->getLyceesForCordee($cordee);
+        
+        // Ajoute un changement de partenariat pour chacun des lycées
+        foreach ($lycees as $lycee) {
+            $this->changerPartenariat($cordee, $lycee);
+        }
         
         $this->get('session')->setFlash('success', 'La cordée a bien été désactivée.');
         return $this->redirect($this->generateUrl('toutes_cordees'));
-    }*/
+    }
     
     /**
      * Affiche tous les lycées inactifs
