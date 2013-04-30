12ffa83304f0f95bba883a41d76d63d7822a73b9
Nanouk <j2b.bayle@gmail.com>
2013-05-01 01:20:38 +0200

    Ajout de la page d'un groupe de tutorat

---
+++ src/CEC/TutoratBundle/Controller/GroupesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CEC\TutoratBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CEC\TutoratBundle\Entity\Groupe;
+
+class GroupesController extends Controller
+{
+    /**
+     * Affiche la page d'un groupe de tutorat.
+     *
+     * @param integer $groupe: id du groupe de tutorat
+     */
+    public function voirAction($groupe)
+    {
+        $groupe = $this->getDoctrine()->getRepository('CECTutoratBundle:Groupe')->find($groupe);
+        if (!$groupe) throw $this->createNotFoundException('Impossible de trouver le groupe de tutorat !');
+            
+        // On rassemble les séances à venir
+        $seances = $this->getDoctrine()->getRepository('CECTutoratBundle:Seance')->findComingByGroupe($groupe);
+        
+        $lyceens = $groupe->getLyceens()->toArray();
+        $tuteurs = $groupe->getTuteurs()->toArray();
+        // On trie les tuteurs et les lycéens par ordre alphabétique
+        usort($tuteurs, function($a, $b) {
+            return strcmp($a->getNom(), $b->getNom());
+        });
+        usort($lyceens, function($a, $b) {
+            return strcmp($a->getNom(), $b->getNom());
+        });
+        
+        return $this->render('CECTutoratBundle:Groupes:voir.html.twig', array(
+            'groupe'       => $groupe,
+            'lyceens'      => $lyceens,
+            'tuteurs'      => $tuteurs,
+            'seances'      => $seances,
+        ));
+    }
+}
