07dc4c6d41f8773e58d2724a0011c8c7fdee7aa3
Nanouk <j2b.bayle@gmail.com>
2013-04-30 23:25:21 +0200

    Modification possible du lycée pour les enseignants

--- src/CEC/TutoratBundle/Form/Type/EnseignantType.php
+++ src/CEC/TutoratBundle/Form/Type/EnseignantType.php
@@ -11,11 +11,23 @@ class EnseignantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('nom')
-            ->add('prenom')
+            ->add('prenom', null, array(
+                'label' => 'Prénom',
+            ))
             ->add('role')
-            ->add('email')
-            ->add('telephonePortable')
-            ->add('telephoneFixe')
+            ->add('lycee', null, array(
+                'label' => 'Lycée associé',
+                'empty_value' => 'Aucun lycée',
+            ))
+            ->add('email', null, array(
+                'label' => 'Adresse Email',
+            ))
+            ->add('telephonePortable', null, array(
+                'label' => 'Téléphone portable',
+            ))
+            ->add('telephoneFixe', null, array(
+                'label' => 'Téléphone fixe',
+            ))
             ->add('commentaires');
     }
     
