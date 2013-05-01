cd86ec9aa45c76340eee49fb96ab7b4646cd522f
Nanouk <j2b.bayle@gmail.com>
2013-05-01 13:02:27 +0200

    Permet l'édition d'un groupe de tutorat

--- src/CEC/TutoratBundle/Controller/GroupesController.php
+++ src/CEC/TutoratBundle/Controller/GroupesController.php
@@ -5,6 +5,8 @@ namespace CEC\TutoratBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use CEC\TutoratBundle\Entity\Groupe;
 
+use CEC\TutoratBundle\Form\Type\GroupeType;
+
 class GroupesController extends Controller
 {
     /**
@@ -35,6 +37,52 @@ class GroupesController extends Controller
             'lyceens'      => $lyceens,
             'tuteurs'      => $tuteurs,
             'seances'      => $seances,
+        ));
+    }
+    
+    /**
+     * Permet l'édition d'un groupe de tutorat.
+     *
+     * @param integer $groupe: id du groupe de tutorat
+     */
+    public function editerAction($groupe)
+    {
+        $groupe = $this->getDoctrine()->getRepository('CECTutoratBundle:Groupe')->find($groupe);
+        if (!$groupe) throw $this->createNotFoundException('Impossible de trouver le groupe de tutorat !');
+            
+        // On rassemble les séances à venir
+        $seances = $this->getDoctrine()->getRepository('CECTutoratBundle:Seance')->findComingByGroupe($groupe);
+        
+        $lyceens = $groupe->getLyceens()->toArray();
+        $tuteurs = $groupe->getTuteurs()->toArray();
+        // On trie les tuteurs et les lycéens par ordre alphabétique
+        usort($tuteurs, function($a, $b) {
+            return strcmp($a->getNom(), $b->getNom());
+        });
+        usort($lyceens, function($a, $b) {
+            return strcmp($a->getNom(), $b->getNom());
+        });
+        
+        $form = $this->createForm(new GroupeType(), $groupe);
+        
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
+            if ($form->isValid())
+            {
+                $this->getDoctrine()->getEntityManager()->flush();
+                return $this->redirect($this->generateUrl('groupe', array('groupe' => $groupe->getId())));
+            }
+        }
+        
+        return $this->render('CECTutoratBundle:Groupes:editer.html.twig', array(
+            'groupe'       => $groupe,
+            'lyceens'      => $lyceens,
+            'tuteurs'      => $tuteurs,
+            'seances'      => $seances,
+            
+            'form'         => $form->createView(),
         ));
     }
 }
---
+++ src/CEC/TutoratBundle/Form/Type/GroupeType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace CEC\TutoratBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GroupeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('lycees', null, array(
+                'label' => 'Lycées associés (pour sélectionner plusieurs lycées, maintenez la touche Ctr enfoncée)',
+            ))
+            ->add('niveau')
+            ->add('typeDeTutorat', 'choice', array(
+                'label' => 'Type de tutorat',
+                'choices' => array(
+                    'Tutorat Scientifique' => 'Tutorat Scientifique', 
+                    'Tutorat Culturel' => 'Tutorat Culturel',
+                    'Tutorat Culturel et Scientifique' => 'Tutorat Culturel et Scientifique',
+                    ),
+                'empty_value' => false,
+            ))
+            ->add('lieu', null, array(
+                'attr' => array('placeholder' => 'A Centrale ou dans le lycée'),
+            ))
+            ->add('debut', null, array(
+                'label' => 'Indiquez la date et l\'heure d\'une séance — seul le jour de la semaine sera pris en compte',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy à hh:mm',
+                'attr' => array('placeholder' => 'JJ/MM/AAAA à HH:MM'),
+            ))
+            ->add('fin', null, array(
+                'label' => 'Indiquez l\'heure de fin de séance — la date étant celle indiquée pour le début de la séance',
+                'widget' => 'single_text',
+                'format' => 'hh:mm',
+                'attr' => array('placeholder' => 'HH:MM'),
+            ))
+            ->add('rendezVous', null, array(
+                'label' => 'Lieu de rendez-vous pour partir en séance',
+                'attr' => array('placeholder' => 'Rendez-vous...'),
+            ));
+    }
+    
+    public function getName()
+    {
+        return 'groupe';
+    }
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CEC\TutoratBundle\Entity\Groupe',
+        ));
+    }
+}
