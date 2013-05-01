83f90daebb362b91d00f43cf9ae25bb199f50d2b
Nanouk <j2b.bayle@gmail.com>
2013-05-01 17:12:24 +0200

    Ajout des liens vers les pages ajoutées dans le menu

--- src/CEC/MainBundle/Controller/MenuController.php
+++ src/CEC/MainBundle/Controller/MenuController.php
@@ -8,5 +8,9 @@ class MenuController extends Controller
 {
     public function menuAction()
     {
+        $membre = $this->getUser();
+        if (!$membre) throw $this->createNotFoundException('Impossible de trouver votre profil');
+        
+        
     }
 }
