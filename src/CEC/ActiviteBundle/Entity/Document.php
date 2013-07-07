ccda3d6137f0671567b3a6a94d1b1f260a9a0e45
Nanouk <j2b.bayle@gmail.com>
2013-07-07 13:30:40 +0200

    Change les fichiers Word en "fichiers originaux", et accepte les PPT

--- src/CEC/ActiviteBundle/Entity/Document.php
+++ src/CEC/ActiviteBundle/Entity/Document.php
@@ -12,9 +12,9 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * et associés à une activité. 
  *
  * Il s'agit d'une version du contenu de l'activité, 
- * en deux formats : le format PDF permettant un téléchargement et une compatibilité
- * étendue, et le format Microsoft Word, permettant l'édition de l'activité si nécessaire
- * (comme par exemple si des corrections sont demandées dans un compte-rendu de l'activité).
+ * en deux formats : le format PDF permettant un téléchargement et une compatibilité étendue,
+ * et le format original (Microsoft Word ou PowerPoint), permettant l'édition de l'activité si
+ * nécessaire(comme par exemple si des corrections sont demandées dans un compte-rendu de l'activité).
  *
  * La classe gère la gestion des fichiers sur le serveur, et, si nécessaire et disponible,
  * la conversion au format PDF grâce au site tierce http://www.conv2pdf.com.
@@ -22,7 +22,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * si la conversion est disponible ; le fichier PDF sera alors automatiquement généré.
  *
  * Il est important de noter que deux documents ne peuvent avoir le même nom sur le serveur ;
- * c'est pourquoi nomFichierPDF et nomFichierWord doivent être uniques. Un historique des changements
+ * c'est pourquoi nomFichierPDF et nomFichierOriginal doivent être uniques. Un historique des changements
  * d'une activité est conservé grâce au champ "description" de chaque document.
  *
  * IMPORTANT : dans la version 1.0, la génération automatique de PDF à partir du fichier Word
@@ -42,8 +42,8 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  *     message = "Un fichier PDF possédant le même nom est déjà présent sur le serveur. Merci de ré-essayer."
  * )
  * @UniqueEntity(
- *     fields = "nomFichierWord",
- *     message = "Un fichier Word possédant le même nom est déjà présent sur le serveur. Merci de ré-essayer."
+ *     fields = "nomFichierOriginal",
+ *     message = "Un fichier original possédant le même nom est déjà présent sur le serveur. Merci de ré-essayer."
  * )
  */
 class Document
@@ -75,29 +75,29 @@ class Document
     private $description;
     
     /**
-     * Fichier Word téléchargé.
+     * Fichier original téléchargé.
      * Cet attribut permet de générer un champ de téléchargement de fichier dans un formulaire
-     * lors de la création du document. Il doit correspondre au fichier Word, et est donc requis.
-     * Il est important de noter que seules les extensions .doc et .docx sont acceptés, et que la 
-     * taille du fichier ne peut excéder 1 Mo.
+     * lors de la création du document. Il doit correspondre au fichier original, et est donc requis.
+     * Il est important de noter que seules les extensions Microsoft Word (.doc et .docx), ainsi que celle
+     * Microsoft PowerPoint (.ppt et .pptx) sont acceptées, et que la taille du fichier ne peut excéder 1 Mo.
      *
      * @var UploadedFile
      *
      * @Assert\File(
      *     maxSize = "1024k",
-     *     maxSizeMessage = "La taille du fichier Word ne peut excéder 1 Mo.",
-     *     mimeTypes = { "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
-     *     mimeTypesMessage = "Le fichier Word doit être sous format Microsoft Word (.doc ou .docx)."
+     *     maxSizeMessage = "La taille du fichier original ne peut excéder 1 Mo.",
+     *     mimeTypes = { "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-powerpoint", "application/vnd.openxmlformats-officedocument.presentationml.presentation" },
+     *     mimeTypesMessage = "Le fichier original doit être sous format Microsoft Word (.doc ou .docx) ou sous le format Microsoft PowerPoint (.ppt ou .pptx)."
      * )
-     * @Assert\NotBlank(message = "Un fichier Word est requis !")
+     * @Assert\NotBlank(message = "Un fichier original est requis !")
      */
-    private $fichierWord;
+    private $fichierOriginal;
     
     /**
      * Fichier PDF téléchargé.
      * Cet attribut permet de générer un champ de téléchargement de fichier dans un formulaire
      * lors de la création du document. Il doit correspondre au fichier PDF, et n'est donc pas requis.
-     * En effet, si le service est disponible, on peut convertir le fichier Word au format PDF et l'utiliser.
+     * En effet, si le service est disponible, on peut convertir le fichier original au format PDF et l'utiliser.
      * Il est important de noter que seule l'extension .pdf est accepté, et que la 
      * taille du fichier ne peut excéder 1 Mo.
      *
@@ -117,21 +117,21 @@ class Document
     private $fichierPDF;
     
     /**
-     * Représente le nom du fichier Word, avec son extension.
+     * Représente le nom du fichier original, avec son extension.
      * Il permet d'accéder par la suite au fichier stocké sur le serveur.
      * Il s'agit d'une chaîne de caractères de moins de 50 caractères, qui doit être
      * non-vide et unique.
      *
      * @var string
      *
-     * @ORM\Column(name = "fichierWord", type = "string", length = 50)
-     * @Assert\NotBlank(message = "Le nom du fichier Word ne peut pas être vide.")
+     * @ORM\Column(name = "nomFichierOriginal", type = "string", length = 50)
+     * @Assert\NotBlank(message = "Le nom du fichier original ne peut pas être vide.")
      * @Assert\MaxLength(
      *     limit = 50,
-     *     message = "Le nom du fichier Word ne peut excéder 50 caractères."
+     *     message = "Le nom du fichier original ne peut excéder 50 caractères."
      * )
      */
-    private $nomFichierWord;
+    private $nomFichierOriginal;
 
     /**
      * Représente le nom du fichier PDF, avec son extension.
@@ -141,7 +141,7 @@ class Document
      *
      * @var string
      *
-     * @ORM\Column(name = "fichierPDF", type = "string", length = 50)
+     * @ORM\Column(name = "nomFichierPDF", type = "string", length = 50)
      * @Assert\NotBlank(message = "Le nom du fichier PDF ne peut pas être vide.")
      * @Assert\MaxLength(
      *     limit = 50,
@@ -217,23 +217,23 @@ class Document
     }
     
     /**
-     * Retourne le chemin relatif du fichier Word.
+     * Retourne le chemin relatif du fichier original.
      *
-     * @return string Chemin relatif du fichier Word.
+     * @return string Chemin relatif du fichier original.
      */
-    public function getCheminWord()
+    public function getCheminOriginal()
     {
-        return $this->getNomFichierWord() ? $this->getDossierTelechargement() . '/' . $this->getNomFichierWord() : null;
+        return $this->getNomFichierOriginal() ? $this->getDossierTelechargement() . '/' . $this->getNomFichierOriginal() : null;
     }
     
     /**
-     * Retourne le chemin absolu du fichier Word.
+     * Retourne le chemin absolu du fichier original.
      *
-     * @return string Chemin absolu du fichier Word.
+     * @return string Chemin absolu du fichier original.
      */
-    public function getCheminAbsoluWord()
+    public function getCheminAbsoluOriginal()
     {
-        return $this->getNomFichierWord() ? $this->getDossierRacineTelechargement() . '/' . $this->getNomFichierWord() : null;
+        return $this->getNomFichierOriginal() ? $this->getDossierRacineTelechargement() . '/' . $this->getNomFichierOriginal() : null;
     }
     
     /**
@@ -258,8 +258,8 @@ class Document
     }
     
     /**
-     * Génère les noms des fichiers Word et PDF.
-     * Cette méthode génère aléatoirement un nom pour les fichiers Word et PDF.
+     * Génère les noms des fichiers original et PDF.
+     * Cette méthode génère aléatoirement un nom pour les fichiers original et PDF.
      * Elle est appelée avant la persistance de l'entité (et avant sa mise-à-jour).
      *
      * @ORM\PrePersist()
@@ -272,9 +272,9 @@ class Document
             $this->setNomFichierPDF(uniqid() . '.' . $this->fichierPDF->guessExtension());
         }
         
-        if (null !== $this->fichierWord)
+        if (null !== $this->fichierOriginal)
         {
-            $this->setNomFichierWord(uniqid() . '.' . $this->fichierWord->guessExtension());
+            $this->setNomFichierOriginal(uniqid() . '.' . $this->fichierOriginal->guessExtension());
         }
     }
     
@@ -290,10 +290,10 @@ class Document
      */
     public function telecharger()
     {
-        if ($this->getFichierWord() !== null)
+        if ($this->getFichierOriginal() !== null)
         {
-            $this->getFichierWord()->move($this->getDossierRacineTelechargement(), $this->getNomFichierWord());
-            unset($this->fichierWord);
+            $this->getFichierOriginal()->move($this->getDossierRacineTelechargement(), $this->getNomFichierOriginal());
+            unset($this->fichierOriginal);
         }
         
         if ($this->getFichierPDF() !== null)
@@ -308,7 +308,7 @@ class Document
     }
     
     /**
-     * Génère le fichier PDF à partir du fichier Word associé à l'entité.
+     * Génère le fichier PDF à partir du fichier original associé à l'entité.
      * Ne fait rien si aucun fichier Word n'existe. Le fichier PDF généré est déplacé sur le serveur
      * suivant l'attribut $nomFichierPDF.
      *
@@ -334,11 +334,10 @@ class Document
             unlink($fichier);
         }
         
-        if ($fichier = $this->getCheminAbsoluWord()) {
+        if ($fichier = $this->getCheminAbsoluOriginal()) {
             unlink($fichier);
         }
     }
-    
     
     /**
      * Description du document.
@@ -414,26 +413,26 @@ class Document
     }
 
     /**
-     * Set nomFichierWord
+     * Set nomFichierOriginal
      *
-     * @param string $nomFichierWord
+     * @param string $nomFichierOriginal
      * @return Document
      */
-    public function setNomFichierWord($nomFichierWord)
+    public function setNomFichierOriginal($nomFichierOriginal)
     {
-        $this->nomFichierWord = $nomFichierWord;
+        $this->nomFichierOriginal = $nomFichierOriginal;
     
         return $this;
     }
 
     /**
-     * Get nomFichierWord
+     * Get nomFichierOriginal
      *
      * @return string 
      */
-    public function getNomFichierWord()
+    public function getNomFichierOriginal()
     {
-        return $this->nomFichierWord;
+        return $this->nomFichierOriginal;
     }
 
     /**
