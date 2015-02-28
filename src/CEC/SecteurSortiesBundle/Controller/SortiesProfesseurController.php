e57b757d2d3800b5c686fc429dc6460b7add715a
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2015-03-01 00:51:26 +0100

    Préparation des pages du module Sorties
    Pages de base pour le module Sorties pour les profs et les élèves.

---
+++ src/CEC/SecteurSortiesBundle/Controller/SortiesProfesseurController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CEC\SecteurSortiesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use CEC\SecteurSortiesBundle\Entity\Sortie;
+use CEC\SecteurSortiesBundle\Form\Type\SortieType;
+use CEC\SecteurSortiesBundle\Form\Type\CRSortieType;
+use CEC\SecteurSortiesBundle\Form\Type\SansCRSortieType;
+
+class SortiesProfesseurController extends Controller
+{
+    /**
+     * Affiche les sorties à venir
+     *
+     * @Template()
+     */
+    public function voirAction()
+    {
+        $now = new \DateTime("now");
+
+        $sorties = $this->getDoctrine()->getRepository('CECSecteurSortiesBundle:Sortie')->findFollowingSorties($now);
+
+        return array(
+            'sorties' => $sorties
+        );
+    }
+
+    /**
+     * Affiche le menu des sorties
+     *
+     * @param Request $request: requête original
+     */
+    public function menuAction($request)
+    {
+        return $this->render('CECSecteurSortiesBundle:SortiesProfesseur:menu.html.twig', array(
+            'request'         => $request,
+        ));
+    }
+
+    /**
+     * Affiche les sorties passées
+     *
+     * @Template()
+     */
+    public function voirAnciennesAction()
+    {
+        $now = new \DateTime("now");
+
+        $sorties = $this->getDoctrine()->getRepository('CECSecteurSortiesBundle:Sortie')->findPreviousSorties($now);
+
+        return array(
+            'sorties' => $sorties,
+        );
+    }
+
+}
