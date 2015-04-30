f21754e6f9d400031b5660937e65f0b7afc8a3a1
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2015-04-30 12:41:44 +0200

    Fin de l'inscription lycéens
    Fichiers pas pris en compte dans le précédent par erreur.

--- src/CEC/SecteurProjetsBundle/Controller/ProjetsController.php
+++ src/CEC/SecteurProjetsBundle/Controller/ProjetsController.php
@@ -4,7 +4,13 @@ namespace CEC\SecteurProjetsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use CEC\SecteurProjetsBundle\Entity\ProjetEleve;
+
 use CEC\SecteurProjetsBundle\Form\ProjetType;
+use CEC\SecteurProjetsBundle\Form\AjouterLyceenType;
+
+use CEC\MainBundle\AnneeScolaire\AnneeScolaire;
 
 
 
@@ -54,8 +60,15 @@ class ProjetsController extends Controller
 	{
 		$projet = $this->getDoctrine()->getRepository('CECSecteurProjetsBundle:Projet')->loadProjet($slug);
 		if (!$projet) throw $this->createNotFoundException('Impossible de trouver ce projet');
+
+		$lyceens = $projet->getInscritsParAnnee()->toArray();
+        $lyceens = array_filter($lyceens, function(ProjetEleve $e){
+            return ($e->getAnneeScolaire() == AnneeScolaire::withDate());
+        });
+        $lyceens = array_map(function(ProjetEleve $e){return $e->getLyceen();}, $lyceens);
 		
 		$form = $this->createForm(new ProjetType(), $projet);
+		$ajouterLyceenForm = $this->createForm(new AjouterLyceenType($projet));
 		
 		$request = $this->getRequest();
 		
@@ -73,7 +86,10 @@ class ProjetsController extends Controller
 			}
 		}
 		
-		return $this->render('CECSecteurProjetsBundle:Projets:editer.html.twig', array('form'=> $form->createView()));
+		return array('form'=> $form->createView(),
+					 'ajouter_lyceen_form'=>$ajouterLyceenForm->createView(),
+					 'projet' => $projet,
+					 'lyceens' => $lyceens);
 	}
 	
 	/**
@@ -151,6 +167,65 @@ class ProjetsController extends Controller
 		
 		return array('projets'=>$projets);
 	}
+
+
+	/**
+     * Retire un lycéen du projet.
+     *
+     * @param string $slug: slug du projet
+     * @param integer $lyceen: id du lycéen
+     */
+    public function supprimerLyceenAction($slug, $lyceen)
+    {
+        $anneeScolaire = AnneeScolaire::withDate();
+        $projet = $this->getDoctrine()->getRepository('CECSecteurProjetsBundle:Projet')->findOneBySlug($slug);
+        if (!$projet) throw $this->createNotFoundException('Impossible de trouver le projet !');
+            
+        $lyceen = $this->getDoctrine()->getRepository('CECMembreBundle:Eleve')->find($lyceen);
+        if (!$lyceen) throw $this->createNotFoundException('Impossible de trouver le lycéen !');
+        $projetLyceen = $this->getDoctrine()->getRepository('CECSecteurProjetsBundle:ProjetEleve')->findOneBy(array('projet' => $projet, 'lyceen' => $lyceen, 'anneeScolaire' => $anneeScolaire));
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($projetLyceen);
+        
+        $em->flush();
+        return $this->redirect($this->generateUrl('editer_projet', array('slug' => $projet->getSlug())));
+    }
+    
+    /**
+     * Ajoute un lycéen au projet
+     *
+     * @param string $slug: slug du projet
+     * @param integer $lyceen: id du lycéen — Variable POST
+     */
+    public function ajouterLyceenAction($slug)
+    {
+        $projet = $this->getDoctrine()->getRepository('CECSecteurProjetsBundle:Projet')->findOneBySlug($slug);
+        if (!$projet) throw $this->createNotFoundException('Impossible de trouver le projet!');
+        // Récupère le lycéen
+        $ajouterLyceenType = new AjouterLyceenType($projet);    // Permet de trouver le nom du formulaire — Attention, ne doit pas
+                                                  // se confondre avec le nom de la route, ajouter_lyceen !
+        $data = $this->getRequest()->get($ajouterLyceenType->getName());
+        if (array_key_exists('lyceen', $data))
+        {
+            $lyceen = $data['lyceen'];
+        } else {
+            $this->get('session')->setFlash('error', 'Merci de spécifier un lycéen à ajouter.');
+            return $this->redirect($this->generateUrl('editer_projet', array('projet' => $projet->getSlug())));
+        }
+        $lyceen = $this->getDoctrine()->getRepository('CECMembreBundle:Eleve')->find($lyceen);
+        if (!$lyceen) throw $this->createNotFoundException('Impossible de trouver le lycéen !');
+        
+        $projetLyceen = new ProjetEleve();
+        $projetLyceen->setProjet($projet);
+        $projetLyceen->setLyceen($lyceen);
+        $projetLyceen->setAnneeScolaire(AnneeScolaire::withDate());
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($projetLyceen);
+        $em->flush();
+        
+        return $this->redirect($this->generateUrl('editer_projet', array('slug' => $projet->getSlug())));
+    }
 	
 	
 	
