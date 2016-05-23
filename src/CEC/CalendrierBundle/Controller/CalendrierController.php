bcddb8f3a13197d4b46cf88e75953642873e3268
pierrefolens <pierre.folens@student.ecp.fr>
2016-05-23 14:19:55 +0200

    Transfert de la page Planning de TutoratBundle vers Calendrier Bundle. Adaptation des routeurs, controlleurs, views en conséquence

--- src/CEC/CalendrierBundle/Controller/CalendrierController.php
+++ src/CEC/CalendrierBundle/Controller/CalendrierController.php
@@ -4,10 +4,18 @@ namespace CEC\CalendrierBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class CalendrierController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('CECCalendrierBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * Affiche le planning de toutes les séances de tutorat.
+     */
+    public function afficherAction()
+    {
+        return $this->render('CECCalendrierBundle::planning.html.twig');
     }
 }
